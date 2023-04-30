edff0a3537d790b9067ac958f5bc247be5415793
Kolade Ayomide <ayomikolade2003@gmail.com>
2023-04-30 09:47:03 +0100

    make listing form user proof

--- pages/create-listing.tsx
+++ pages/create-listing.tsx
@@ -1,10 +1,13 @@
 import CreateListingForm from "@/components/Listings/CreateListingForm";
+import { auth } from "@/firebase/clientApp";
 import Head from "next/head";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 type CreateListingProps = {};
 
 const CreateListing: React.FC<CreateListingProps> = () => {
+  const [user] = useAuthState(auth);
   return (
     <>
       <Head>
@@ -13,7 +16,7 @@ const CreateListing: React.FC<CreateListingProps> = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <CreateListingForm />
+      {user && <CreateListingForm user={user} />}
     </>
   );
 };
